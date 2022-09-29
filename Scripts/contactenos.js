d9700c06fea1f406e793734c36185855922d0a57
Juanless <estebans1419@gmail.com>
2022-09-29 07:41:00 -0500

    footer

---
+++ Scripts/contactenos.js
@@ -0,0 +1,13 @@
+const ham = document.querySelector('.ham');
+const enlaces = document.querySelector('.enlaces-menu');
+const barras = document.querySelectorAll('.ham span');
+const login = document.querySelector('.button');
+
+ham.addEventListener('click', () => {
+    enlaces.classList.toggle('activado');
+    barras.forEach(child => {child.classList.toggle('animado')});
+});
+
+ham.addEventListener('click', () => {
+    login.classList.toggle('Login');
+});
